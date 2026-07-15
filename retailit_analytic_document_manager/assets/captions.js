b090889b02f104afaa2190f51333990b5e5e001a
Sheldon-Retail-IT <sheldon@retailit.tech>
2026-07-15 08:01:17 +0200

    Add files via upload

--- retailit_analytic_document_manager/assets/captions.js
+++ retailit_analytic_document_manager/assets/captions.js
@@ -4,5 +4,16 @@
 // Leave a value as "" to show no caption for that image.
 
 const MANUAL_CAPTIONS = {
-    // "example_screenshot.png": "Caption text here."
+    "01_config_menu_path.png": "",
+    "02_config_mapping_form.png": "",
+    "03_config_sweep_menu.png": "",
+    "04_so_field.png": "",
+    "05_so_apply_button.png": "",
+    "06_po_field.png": "",
+    "07_po_apply_button.png": "",
+    "08_invoice_apply_button.png": "",
+    "09_invoice_wizard.png": "",
+    "10_bank_multiselect.png": "",
+    "11_bank_toolbar.png": "",
+    "12_bank_wizard.png": ""
 };
